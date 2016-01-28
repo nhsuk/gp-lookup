2e0a962ba029d445d551cd591b0c4246e6ff6703
Dominic Baggott <dominic.baggott@gmail.com>
2016-01-28 13:12:21 +0000

    Refactor PracticeSearchIndex#find
    This commit is a no-op as far as functionality goes.
    
    Stop calling the Blurrily::Map `map`, because it's a slightly overloaded
    term. Call it `practices_haystack` so we can easily add more haystacks
    (ie practitioners).
    
    Rearrange some of the methods to make it easy to add new methods for
    dealing with these soon-to-exist haystacks.

--- lib/practice_search_index.rb
+++ lib/practice_search_index.rb
@@ -8,21 +8,22 @@ class PracticeSearchIndex
   end
 
   def find(search_term)
-    map.find(search_term).map { |index, matches, weight|
-      practices.fetch(index).merge(
-        score: {
-          matches: matches,
-          weight: weight,
-        },
-      )
+    search_haystacks(search_term).sort_by { |result|
+      -result.fetch(:score).fetch(:matches)
     }
   end
 
+  def search_haystacks(search_term)
+    [
+      find_practices(search_term),
+    ].reduce(:+)
+  end
+
 private
-  attr_reader :practices, :map
+  attr_reader :practices, :practices_haystack
 
   def build_index
-    @map = Blurrily::Map.new
+    @practices_haystack = Blurrily::Map.new
 
     practices.each.with_index do |practice, index|
       needle = [
@@ -30,7 +31,18 @@ private
         practice.fetch("address"),
       ].join(", ")
 
-      map.put(needle, index)
+      practices_haystack.put(needle, index)
     end
+  end
+
+  def find_practices(search_term)
+    practices_haystack.find(search_term).map { |index, matches, weight|
+      practices.fetch(index).merge(
+        score: {
+          matches: matches,
+          weight: weight,
+        },
+      )
+    }
   end
 end
