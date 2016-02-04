cb8f0ce0b2176880981acecc7e594dfa8913d4ef
Dominic Baggott <dominic.baggott@gmail.com>
2016-02-04 14:10:12 +0000

    Add a content page for starting the journey

--- server.rb
+++ server.rb
@@ -53,6 +53,10 @@ get "/practices" do
   JSON.pretty_generate(practices)
 end
 
+get "/type-2-diabetes/going-for-regular-check-ups" do
+  erb :regular_check_ups
+end
+
 helpers do
   def react_component(component_name, props = {})
     renderer = React::ExecJSRenderer.new(
