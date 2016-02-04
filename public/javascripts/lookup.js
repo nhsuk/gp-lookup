92d65c536d17821a56a59fa9b39a69b645a8744c
Dominic Baggott <dominic.baggott@gmail.com>
2016-02-04 14:09:44 +0000

    Stop using jQuery to attach React components

--- public/javascripts/lookup.js
+++ public/javascripts/lookup.js
@@ -1,19 +1,27 @@
 "use strict";
 
-$("[data-react-class]").each(function() {
-  var componentName = this.getAttribute("data-react-class"),
-      constructor = window[componentName],
-      propsJson = this.getAttribute("data-react-props"),
-      props = propsJson && JSON.parse(propsJson);
+function attachReactComponents() {
+  var nodes = document.querySelectorAll("[data-react-class]"),
+      index;
 
-  ReactDOM.render(
-    React.createElement(
-      constructor,
-      props
-    ),
-    this
-  );
-});
+  for (index = 0; index < nodes.length; ++index) {
+    var node = nodes[index],
+        componentName = node.getAttribute("data-react-class"),
+        constructor = window[componentName],
+        propsJson = node.getAttribute("data-react-props"),
+        props = propsJson && JSON.parse(propsJson);
+
+    ReactDOM.render(
+      React.createElement(
+        constructor,
+        props
+      ),
+      node
+    );
+  }
+}
+
+attachReactComponents();
 
 var currentAjaxRequest = null;
 
