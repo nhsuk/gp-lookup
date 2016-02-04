18000eccb13f66156ebb2229dad5989224f9b664
Dominic Baggott <dominic.baggott@gmail.com>
2016-02-04 16:32:13 +0000

    Prefix practitioner names with "Dr."

--- public/javascripts/components.js
+++ public/javascripts/components.js
@@ -136,7 +136,7 @@ var PracticeResult = React.createClass({
         "p",
         { className: "person" },
         React.createElement("img", { src: "/images/person-placeholder.svg", width: "45", height: "45", alt: "" }),
-        " ",
+        " Dr. ",
         React.createElement("span", { dangerouslySetInnerHTML: this.highlightText(practitioner.value, practitioner.matches) })
       );
     }).bind(this)),
